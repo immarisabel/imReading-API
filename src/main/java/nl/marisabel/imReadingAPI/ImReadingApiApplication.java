7406e0efa439c50ac3012ebfaa309557e241a781
Marisabel <72731147+immarisabel@users.noreply.github.com>
2023-08-16 20:58:01 +0200

    adding initial spring boot template

---
+++ src/main/java/nl/marisabel/imReadingAPI/ImReadingApiApplication.java
@@ -0,0 +1,13 @@
+package nl.marisabel.imReadingAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImReadingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImReadingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/marisabel/imReadingAPI/ImReadingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.marisabel.imReadingAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImReadingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
