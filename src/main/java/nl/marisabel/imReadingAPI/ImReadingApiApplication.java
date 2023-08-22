ca5f38a0d59ee09377debc1f7a26b4712ad06710
Marisabel <72731147+immarisabel@users.noreply.github.com>
2023-08-22 12:29:43 +0200

    google search API key is working
    RESPONSE:
    http://localhost:8080/search?query=dune
    
    [
        {
            "title": "Dune",
            "author": "Frank Herbert",
            "isbn": "9781101658055",
            "thumbnail": "http://books.google.com/books/content?id=p1MULH7JsTQC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api",
            "pageCount": 898,
            "selfLink": "https://www.googleapis.com/books/v1/volumes/p1MULH7JsTQC"
        },...

--- src/main/java/nl/marisabel/imReadingAPI/ImReadingApiApplication.java
+++ src/main/java/nl/marisabel/imReadingAPI/ImReadingApiApplication.java
@@ -1,29 +1,19 @@
 package nl.marisabel.imReadingAPI;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
-import javax.sql.DataSource;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ImReadingApiApplication {
-	@Autowired
-	Environment env;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ImReadingApiApplication.class, args);
 	}
-
 	@Bean
-	public DataSource dataSource() {
-		final DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName(env.getProperty("driverClassName"));
-		dataSource.setUrl(env.getProperty("url"));
-		dataSource.setUsername(env.getProperty("user"));
-		dataSource.setPassword(env.getProperty("password"));
-		return dataSource;
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 }
+
--- src/main/java/nl/marisabel/imReadingAPI/searchApi/GoogleBooksApiService.java
+++ src/main/java/nl/marisabel/imReadingAPI/searchApi/GoogleBooksApiService.java
@@ -11,7 +11,6 @@
 package nl.marisabel.imReadingAPI.searchApi;
 
 import lombok.extern.java.Log;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -22,21 +21,20 @@ import java.io.IOException;
 @Log
 public class GoogleBooksApiService {
 
- @Autowired
  private RestTemplate restTemplate;
+ private final String apiKey;
 
- public GoogleBooksApiService(RestTemplate restTemplate) {
+ public GoogleBooksApiService(RestTemplate restTemplate, @Value("${keys.gBooks}") String apiKey) {
   this.restTemplate = restTemplate;
+  this.apiKey = apiKey;
  }
-
-    @Value("${keys.gBooks}")
-    private String apiKey;
 
 
  // Generate url with ISBN and key
  public String getApiUri(String query) throws IOException {
 
-  String apiKey = System.getenv("$GOOGLEBOOKS_APIKEY");
+
+  System.out.println(apiKey);
 
   return "https://www.googleapis.com/books/v1/volumes?q=" + query + "&download=epub&key=" + apiKey + "&maxResults=20";
 
