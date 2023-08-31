6bf4adb436d0b95a54653a2adf4ef0e8934ce9fe
Marisabel <72731147+immarisabel@users.noreply.github.com>
2023-08-31 10:52:29 +0200

    adding response handler

---
+++ src/main/java/nl/marisabel/imReadingAPI/responses/ResponseHandler.java
@@ -0,0 +1,28 @@
+/*
+ *
+ *  * imReading API
+ *  * Copyright (c) 2023 Marisabel Munoz
+ *  * This project is licensed under the terms of the MIT License.
+ *  * For more information, please see the https://opensource.org/license/mit/.
+ *
+ *
+ */
+
+package nl.marisabel.imReadingAPI.responses;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ResponseHandler {
+ public static ResponseEntity<Object> generateResponse(String message, HttpStatus status, Object responseObj) {
+  Map<String, Object> map = new HashMap<>();
+  map.put("message", message);
+  map.put("status", status.value());
+  map.put("data", responseObj);
+
+  return new ResponseEntity<>(map, status);
+ }
+}
